f3bc94aa291e43056b3e01ae31b4507c8bcf51bf
tosher <to.tosher@gmail.com>
2015-03-06 13:55:28 +0300

    selected text for language links

--- mediawiker.py
+++ mediawiker.py
@@ -181,6 +181,12 @@ class MediawikerInsertTextCommand(sublime_plugin.TextCommand):
         self.view.insert(edit, position, text)
 
 
+class MediawikerReplaceTextCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, text):
+        self.view.replace(edit, self.view.sel()[0], text)
+
+
 class MediawikerPageCommand(sublime_plugin.WindowCommand):
     '''prepare all actions with wiki'''
 
@@ -1225,6 +1231,10 @@ class MediawikerPageLanglinksCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, title, password):
         sitecon = mw_get_connect(password)
+        # selection = self.view.sel()
+        # search_pre = self.view.substr(selection[0]).strip()
+        selected_text = self.view.substr(self.view.sel()[0]).strip()
+        title = selected_text if selected_text else title
         self.mw_get_page_langlinks(sitecon, title)
 
         self.lang_prefixes = []
@@ -1232,7 +1242,10 @@ class MediawikerPageLanglinksCommand(sublime_plugin.TextCommand):
             self.lang_prefixes.append(lang_prefix)
 
         self.links_names = ['%s: %s' % (lp, self.links[lp]) for lp in self.lang_prefixes]
-        sublime.active_window().show_quick_panel(self.links_names, self.on_done)
+        if self.links_names:
+            sublime.active_window().show_quick_panel(self.links_names, self.on_done)
+        else:
+            sublime.status_message('Unable to find laguage links for "%s"' % title)
 
     def mw_get_page_langlinks(self, site, title):
         self.links = {}
@@ -1248,16 +1261,21 @@ class MediawikerPageLanglinksCommand(sublime_plugin.TextCommand):
 
     def on_done(self, index):
         if index >= 0:
-            lang_prefix = self.lang_prefixes[index]
-            page_name = self.links[lang_prefix]
+            self.lang_prefix = self.lang_prefixes[index]
+            self.page_name = self.links[self.lang_prefix]
 
+            self.process_options = ['Open selected page', 'Replace selected text']
+            sublime.active_window().show_quick_panel(self.process_options, self.process)
+
+    def process(self, index):
+        if index == 0:
             site_active_new = None
             site_active = mw.get_view_site()
             sites = mw.get_setting('mediawiki_site')
             host = sites[site_active]['host']
             domain_first = '.'.join(host.split('.')[-2:])
             # NOTE: only links like lang_prefix.site.com supported.. (like en.wikipedia.org)
-            host_new = '%s.%s' % (lang_prefix, domain_first)
+            host_new = '%s.%s' % (self.lang_prefix, domain_first)
             # if host_new exists in settings we can open page
             for site in sites:
                 if sites[site]['host'] == host_new:
@@ -1265,6 +1283,8 @@ class MediawikerPageLanglinksCommand(sublime_plugin.TextCommand):
                     break
             if site_active_new:
                 # open page with force site_active_new
-                sublime.active_window().run_command("mediawiker_page", {"title": page_name, "action": "mediawiker_show_page", "site_active": site_active_new})
+                sublime.active_window().run_command("mediawiker_page", {"title": self.page_name, "action": "mediawiker_show_page", "site_active": site_active_new})
             else:
                 sublime.status_message('Settings not found for host %s.' % (host_new))
+        elif index == 1:
+            self.view.run_command('mediawiker_replace_text', {'text': self.page_name})
