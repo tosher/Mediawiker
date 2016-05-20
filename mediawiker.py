96c162b31f23c4d82c961f8a87aa7201f87fefe0
tosher <to.tosher@gmail.com>
2016-05-21 00:34:51 +0300

    v3.1.2 Page preview: geshi highlighting, custom head tags

--- mediawiker.py
+++ mediawiker.py
@@ -1420,8 +1420,9 @@ class MediawikerPreviewPageCommand(sublime_plugin.TextCommand):
 
         site_active = mw.get_view_site()
         site_list = mw.get_setting('mediawiki_site')
-        site = site_list[site_active]['host']
+        host = site_list[site_active]['host']
         path = site_list[site_active]['path']
+        head = '\n'.join(site_list[site_active].get('preview_custom_head', []))
         lang = mw.get_setting('mediawiki_preview_lang', 'en')
         preview_file = mw.get_setting('mediawiki_preview_file', 'Wiki_page_preview_')
         site_http = 'https' if site_list[site_active].get('https', False) else 'http'
@@ -1431,18 +1432,21 @@ class MediawikerPreviewPageCommand(sublime_plugin.TextCommand):
             '<html>',
             '<head>',
             '<meta charset="UTF-8"/>',
-            '<link rel="stylesheet" href="%(http)s://%(site)s%(path)sload.php?debug=false&amp;lang=%(lang)s&amp;modules=site&amp;only=styles&amp;skin=vector"/>',
-            '<link rel="stylesheet" href="%(http)s://%(site)s%(path)sload.php?debug=false&amp;lang=%(lang)s&amp;modules=ext.visualEditor.viewPageTarget.noscript|mediawiki.legacy.commonPrint,shared|mediawiki.sectionAnchor|mediawiki.skinning.interface|mediawiki.ui.button|skins.vector.styles&amp;only=styles&amp;skin=vector&amp;*" />',
+            '<link rel="stylesheet" href="%(http)s://%(host)s%(path)sload.php?debug=false&amp;lang=%(lang)s&amp;modules=site&amp;only=styles&amp;skin=vector"/>',
+            '<link rel="stylesheet" href="%(http)s://%(host)s%(path)sload.php?debug=false&amp;lang=%(lang)s&amp;modules=%(geshi_css)sext.visualEditor.viewPageTarget.noscript|mediawiki.legacy.commonPrint,shared|mediawiki.sectionAnchor|mediawiki.skinning.interface|mediawiki.ui.button|skins.vector.styles&amp;only=styles&amp;skin=vector&amp;*" />',
+            '%(head)s',
             '</head>',
             '<body style="margin:20px;">'
         ]
 
-        html_header = '\n'.join(html_header_lines) % {'http': site_http, 'site': site, 'path': path, 'lang': lang}
+        geshi_css = self.get_geshi_langs(text)
+        head = head % {'http': site_http, 'host': host, 'path': path, 'lang': lang}
+        html_header = '\n'.join(html_header_lines) % {'http': site_http, 'host': host, 'path': path, 'lang': lang, 'geshi_css': geshi_css, 'head': head}
         html_footer = '</body></html>'
 
         html = sitecon.parse(text=text, title=mw.get_title(), disableeditsection=True).get('text', {}).get('*', '')
         html = html.replace('"//', '"%s://' % site_http)  # internal links: images,..
-        html = html.replace('"/', '"%s://%s/' % (site_http, site))  # internal local links: images,..
+        html = html.replace('"/', '"%s://%s/' % (site_http, host))  # internal local links: images,..
 
         if preview_file.endswith('.html'):
             preview_file = join(sublime.packages_path(), 'User', preview_file)
@@ -1476,6 +1480,15 @@ class MediawikerPreviewPageCommand(sublime_plugin.TextCommand):
                     tf.write(html.encode('utf-8'))
                     tf.write(html_footer)
             webbrowser.open(tf.name)
+
+    def get_geshi_langs(self, text):
+        langs = []
+        pattern = r'<source lang="(.*?)"(.*?)>'
+        self.regions = self.view.find_all(pattern)
+        for r in self.regions:
+            lang = re.sub(pattern, r'\1', self.view.substr(r))
+            langs.append(lang)
+        return 'ext.geshi.language.%s|' % ','.join(set(langs)) if langs else ''
 
 
 class MediawikerOpenIssueCommand(sublime_plugin.WindowCommand):
